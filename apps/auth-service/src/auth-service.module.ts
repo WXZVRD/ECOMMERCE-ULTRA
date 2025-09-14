1696df8a99469ea8f04b1b3a561db040f3aa92da
wxzvd <forworknickworron@gmail.com>
2025-09-14 16:38:31 +0500

    Add: injected and adjusted typeorm module

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {getDbConfig} from "./configs/database.config";
 
 @Module({
   imports: [
       ConfigModule.forRoot({
           isGlobal: true,
           envFilePath: './apps/auth-service/.env'
+      }),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: getDbConfig,
       })
   ],
   controllers: [AuthServiceController],
