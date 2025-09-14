578d1738d2a25a84b6117ee85661a8a983d799ea
wxzvd <forworknickworron@gmail.com>
2025-09-14 23:58:37 +0500

    Add: test login and register methods

--- apps/auth-service/src/modules/auth/auth.controller.ts
+++ apps/auth-service/src/modules/auth/auth.controller.ts
@@ -1,8 +1,19 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Post} from "@nestjs/common";
 import {AuthService} from "./auth.service";
 
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+
+
+    @Post('login')
+    async login(): Promise<string> {
+        return this.authService.login()
+    }
+
+    @Post('register')
+    async register(): Promise<string> {
+        return this.authService.register()
+    }
 }
