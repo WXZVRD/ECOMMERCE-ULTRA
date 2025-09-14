4684895762c7305825168a9aca824359132a206f
wxzvd <forworknickworron@gmail.com>
2025-09-14 16:27:27 +0500

    Add: adjusted config module

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: './apps/auth-service/.env'
+      })
+  ],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
