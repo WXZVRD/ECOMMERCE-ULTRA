5ac10d8bea572ec640dd062e3ef9b5001f8c3798
wxzvd <forworknickworron@gmail.com>
2025-09-15 22:08:34 +0500

    [USER-SERVICE] Update: inject TypeOrmModule and ConfigModule

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserServiceController } from './user-service.controller';
 import { UserServiceService } from './user-service.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {getDbConfig} from "./configs";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: './apps/user-service/.env'
+  }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getDbConfig,
+    }),],
   controllers: [UserServiceController],
   providers: [UserServiceService],
 })
